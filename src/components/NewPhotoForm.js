4c9efa5fc6d8ed1a40a29fdd0dd235ef368188b3
Brigit Rossbach <brigitrossbach@gmail.com>
2017-10-10 15:01:10 -0400

    uploading photos and getting back tagged photos works

---
+++ src/components/NewPhotoForm.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+class NewPhotoForm extends React.Component {
+
+  state={
+    file: '',
+    imagePreviewUrl: ''
+  }
+
+  handleChange = (event) => {
+    event.preventDefault()
+
+    let reader = new FileReader()
+    let file = event.target.files[0]
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      })
+    }
+    reader.readAsDataURL(file)
+   }
+
+  handleSubmit= (event) => {
+    event.preventDefault()
+    let form = event.target.parentElement
+    let data=new FormData(form)
+    data.append('image', this.state.imagePreviewUrl)
+    fetch('http://localhost:3000/photos', {
+      method: 'POST',
+      body: data
+    })
+    .then(resp => resp.json())
+    .then(json => console.log(json))
+  }
+
+  render(){
+    let {imagePreviewUrl} = this.state
+    let $imagePreview = null
+    if (imagePreviewUrl){
+      $imagePreview = (<img className='image-preview' alt='uploaded'src={imagePreviewUrl} />)
+    }
+
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type='file' accept='image/*' onChange={this.handleChange} />
+          <button type="submit" onClick={this.handleSubmit}>Upload Image</button>
+        </form>
+        {$imagePreview}
+      </div>
+    )
+  }
+}
+
+export default NewPhotoForm
--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { BrowserRouter } from 'react-router-dom'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({})
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+ReactDOM.render(<Provider store={store}/><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
 registerServiceWorker();
+
+
+
+//in actions- want to export a function that returns a function that takes dispatch
+//then fetches the books and dispatches the fetched books
