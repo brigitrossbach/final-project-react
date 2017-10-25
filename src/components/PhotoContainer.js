f6286a1833f08420ef2b1a818512902575a5d1f2
Brigit Rossbach <brigitrossbach@gmail.com>
2017-10-25 16:50:54 -0400

    added loading screen

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -84,9 +84,10 @@ class PhotoContainer extends React.Component {
       )
     } else {
       return(
-        <div></div>
+        <div><iframe src="https://giphy.com/embed/3oEjI6SIIHBdRxXI40" width="480" height="480" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/mashable-3oEjI6SIIHBdRxXI40">via GIPHY</a></p></div>
       )
     }
+
   }
 }
 
--- src/components/ProfilePhotoList.js
+++ src/components/ProfilePhotoList.js
@@ -16,7 +16,7 @@ class ProfilePhotoList extends React.Component {
 
     } else {
     return(
-      <div>Follow a user to see their photos.</div>
+      <div><iframe src="https://giphy.com/embed/3oEjI6SIIHBdRxXI40" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/mashable-3oEjI6SIIHBdRxXI40">via GIPHY</a></p></div>
     )
   }
   }
